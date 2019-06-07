ab3f3f6a5dfe093d1124ec1074df4d56fc585d7f
Claudia Falk <claudiafalk0@gmail.com>
2019-06-06 20:40:01 -0600

    added for loop and while loop to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,31 @@
 window.onload = function (){
-   // $("#submit").on("click", function);
+   $("#submit").on("click", submit);
     $("#start").on("click", start);
 
 }
+var images =  { CorrectGuess:[{ name: "fairy", img: "../images/fairy.jpg"}, 
+{name: "dragon", img: "../images/dragon.jpg"}, {name: "centaur", img: "../images/centaur.jpg"}, {name: "gnome", img: "../images/gnome.jpg"}
+, { name: "mermaid", img: "../images/mermaid.jpg"}]};
+var wrongAnswer = ["griffin", "pixie", "basilisk", "selkie", "ghoul", "werewolf", "vampire"];
 var timer;
 var score;
 var time = 120;
+var Submitted = false;
 //reset the game 
+
+
 function reset (){
     $("#display").text("0:00");
     $("#start").show;
     score = 0;
     time = 120;
 }
-//start the timer, display all questions, remove start button
+//start the timer, display a question, remove start button
 function start (){
     $("#start").hide();
     $("#game").show();
     timer = setInterval(count, 1000);
+    trivia();
 }
 
 function count (){
@@ -25,6 +33,24 @@ function count (){
     var converted = timeConverter(time);
     $("#display").text(converted);
 }
+//display question with three different answer choices (1 correct, 2 incorrect)
+function trivia(){
+    for(var i = 0; i <images.length; i++){
+        var random = wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)];
+        $("#question").html("<img src = " + images.CorrectGuess[i].img + "/>");
+        $("#choice1").html(images.CorrectGuess[i].name);
+        $("#choice2").text(random);
+        $("#choice3").text(random);
+    }
+
+}
+console.log($("#question"));
+console.log($("#choice1"));
+//save answer chosen, change questions after click, once all questions have been submitted, stop timer, show #correct/correct answers
+// function submit (){
+//     $("#submit").on("click", function())
+
+// }
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
