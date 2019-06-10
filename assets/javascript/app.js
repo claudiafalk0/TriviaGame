6e3f676212c577361f8a31b1360a72d9ddcf1ade
Claudia Falk <claudiafalk0@gmail.com>
2019-06-09 21:23:32 -0600

    update generation logic, add giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,19 @@
-window.onload = function (){
-   $("#buttons").on("click", submit);
-    $("#start").on("click", start);
+// ![Advanced](Images/2-advanced.jpg)
 
-}
+// **[Click Here to Watch the demo](https://youtu.be/xhmmiRmxQ8Q)**.
+
+// * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+// * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+// * The scenario is similar for wrong answers and time-outs.
+
+//   * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+//   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
+// * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
 var images =  { CorrectGuess:[
     {name: "fairy", img: "./assets/images/fairy.jpg"}, 
     {name: "dragon", img: "./assets/images/dragon.jpg"}, 
@@ -13,16 +24,19 @@ var images =  { CorrectGuess:[
 var wrongAnswer = ["griffin", "pixie", "basilisk", "selkie", "ghoul", "werewolf", "vampire"];
 var timer;
 var score;
-var time = 120;
+var time = 30;
 var Submitted = false;
+var position;
+var giphy = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TEjv4DhokKiGmbR0CqNmeMPDL5lC9h6u");
+giphy.done(function(data) { console.log("success got data", data); });
+				
 //reset the game 
 
-
 function reset (){
-    $("#display").text("2:00");
+    $("#display").text("0:30");
     $("#start").show;
     score = 0;
-    time = 120;
+    time = 30;
 }
 //start the timer, display a question, remove start button
 function start (){
@@ -32,36 +46,50 @@ function start (){
     trivia();
 }
 
+$("#start").on("click", function (){
+    start();
+})
+
 function count (){
-    time--;
-    var converted = timeConverter(time);
-    $("#display").text(converted);
+    if(time > 0) {
+        time--;
+        var converted = timeConverter(time);
+        $("#display").text(converted);
+    }
 }
 //display question with three different answer choices (1 correct, 2 incorrect)
 function trivia(){
-    if(!Submitted){
-        for(var i = 0; i < images.CorrectGuess.length; i++){
-            var temp = new Image();
-            temp.src = images.CorrectGuess[i].img;
-            $("#question").html("<img id=\"img_" + i + "\"" + "/>");
-            $("#img_" + i).attr("src", temp.src);
-            $("#choice1").html(images.CorrectGuess[i].name);
-            $("#choice2").text( wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)]);
-            $("#choice3").text( wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)]);
-            submit();
-        }
-    }
+if(!Submitted){
+    position = Math.floor(Math.random() * images.CorrectGuess.length);
+    var temp = new Image();
+    temp.src = images.CorrectGuess[position].img;
+    $("#question").html("<img id=\"img\"/>");
+    $("#img").attr("src", temp.src);
+
+    //Determine which position the correct answer will be in
+    var RN = Math.floor(Math.random() * 3);
+
+    //Set the choices based on the correct answer position
+    $("#choice1").text(RN === 1 ? images.CorrectGuess[position].name : wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)]);
+    $("#choice2").text(RN === 2 ? images.CorrectGuess[position].name : wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)]);
+    $("#choice3").text(RN === 3 ? images.CorrectGuess[position].name : wrongAnswer[Math.floor(Math.random()*wrongAnswer.length)]);
+}
 
 }
-console.log($("#question"));
-console.log($("#choice1"));
+
 //save answer chosen, change questions after click, once all questions have been submitted, stop timer, show #correct/correct answers
-function submit (){
-    $("#buttons").on("click", function(){
-            Submitted = false;
-    })
+$("#choice1").on("click", function(){
+    if($("#choice1").text() === images.CorrectGuess[position].name){
 
-}
+    }
+});
+$("#choice2").on("click", function(){
+    alert($(this).text());
+});
+$("#choice3").on("click", function(){
+    alert($(this).text());
+});
+
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
